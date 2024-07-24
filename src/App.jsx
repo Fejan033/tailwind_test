0de090cf5f8aed314d1b54e22fc67753145643ec
Mohammed <fejan033@gmail.com>
2024-07-24 22:37:34 +0530

    Tailwind_Added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Body from "./Components/Body";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+
+function App() {
+  return (
+    <div className="bg-amber-100">
+      <Header />
+      <Footer />
+      <Body />
+    </div>
+  );
+}
+
+export default App;
