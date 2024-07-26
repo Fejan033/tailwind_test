95e33b1fd35ae1069fb676fb68d546554033df77
Mohammed <fejan033@gmail.com>
2024-07-26 07:07:38 +0530

    Body_Setted

--- src/Components/Body.jsx
+++ src/Components/Body.jsx
@@ -4,13 +4,42 @@ const Body = () => {
   return (
     <div>
       <div className="flex items-center justify-center ">
-        <img src="./assets/Blue-Shape.svg" alt="1" className="absolute"/>
-        <img src="./assets/Pink-Shape.svg" alt="2" className="absolute" />
-        <img src="./assets/Purple-Shape.svg" alt="3" className="absolute"/>
-        <img src="./assets/Hero-Model.png" alt="4" className="absolute"/>
+        <img
+          src="./assets/Blue-Shape.svg"
+          alt="1st"
+          className=" -rotate-45 h-64"
+        />
+        <img
+          src="./assets/Pink-Shape.svg"
+          alt="2nd"
+          className="absolute -rotate-[30deg] h-64"
+        />
+        <img
+          src="./assets/Purple-Shape.svg"
+          alt="3rd"
+          className="absolute -rotate-[15deg] h-64"
+        />
+        <img
+          src="./assets/Hero-Model.png"
+          alt="Hero"
+          className="absolute h-64"
+        />
       </div>
 
-      <div></div>
+      <div>
+        <h1 className="text-5xl font-bold font-playfair leading-tight">Welcome to the world first and fastest hoster for your web</h1>
+        <p className="font-playfair text-gray-400 ">
+          Experience unparalleled speed and reliability with our cutting-edge
+          web hosting solutions.
+        </p>
+        <form action="" className="flex flex-col gap rounded-md px-4 py-3 bg-gray-100 ">
+          <input type="email" placeholder="let us know your mail here" />
+          <button>Join Us</button>
+        </form>
+        <div>
+          <img src="./assets/Checkmark.svg" alt="check" />
+        </div>
+      </div>
     </div>
   );
 };
